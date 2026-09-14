a1718a9dacc843c95c29b5429281c7d043a0549d
Utopia <sindhra199@gmail.com>
2026-09-14 20:09:43 +0700

    update(doom): add tree-sitter to every lang modules

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -21,12 +21,12 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       ;; company           ; the ultimate code completion backend
+       ;; company          ; the ultimate code completion backend
        (corfu +orderless)  ; complete with cap(f), cape and a flying feather!
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       vertico           ; the search engine of the future
+       vertico             ; the search engine of the future
 
        :ui
        ;;deft              ; notational velocity for Emacs
@@ -71,21 +71,21 @@
        :emacs
        (dired +dirvish +icons)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
-       ;;eww               ; the internet is gross
+       ;;eww             ; the internet is gross
        ibuffer           ; interactive buffer management
        tramp             ; remote files at your arthritic fingertips
        undo              ; persistent, smarter undo for your inevitable mistakes
        vc                ; version-control and Emacs, sitting in a tree
 
        :term
-       eshell            ; the elisp shell that works everywhere
+       eshell              ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
        ;;vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
-       (spell +flyspell) ; tasing you for misspelling mispelling
+       (spell +flyspell)   ; tasing you for misspelling mispelling
        ;;grammar           ; tasing grammar mistake every you make
 
        :tools
@@ -115,72 +115,72 @@
        ;;tty               ; improve the terminal Emacs experience
 
        :lang
-       ;;ada               ; In strong typing we (blindly) trust
-       ;;(agda +local)     ; types of types of types of types...
-       ;;beancount         ; mind the GAAP
-       (cc +lsp)         ; C > C++ == 1
-       ;;clojure           ; java with a lisp
-       ;;common-lisp       ; if you've seen one lisp, you've seen them all
-       ;;coq               ; proofs-as-programs
-       ;;crystal           ; ruby at the speed of c
-       ;;csharp            ; unity, .NET, and mono shenanigans
-       ;;data              ; config/data formats
-       ;;(dart +flutter)   ; paint ui and not much else
+       ;;ada                           ; In strong typing we (blindly) trust
+       ;;(agda +local)                 ; types of types of types of types...
+       ;;beancount                     ; mind the GAAP
+       (cc +lsp +tree-sitter)          ; C > C++ == 1
+       ;;clojure                       ; java with a lisp
+       ;;common-lisp                   ; if you've seen one lisp, you've seen them all
+       ;;coq                           ; proofs-as-programs
+       ;;crystal                       ; ruby at the speed of c
+       ;;csharp                        ; unity, .NET, and mono shenanigans
+       ;;data                          ; config/data formats
+       ;;(dart +flutter)               ; paint ui and not much else
        ;;dhall
-       ;;elixir            ; erlang done right
-       ;;elm               ; care for a cup of TEA?
-       emacs-lisp        ; drown in parentheses
-       ;;erlang            ; an elegant language for a more civilized age
-       ;;ess               ; emacs speaks statistics
+       ;;elixir                        ; erlang done right
+       ;;elm                           ; care for a cup of TEA?
+       emacs-lisp                      ; drown in parentheses
+       ;;erlang                        ; an elegant language for a more civilized age
+       ;;ess                           ; emacs speaks statistics
        ;;factor
-       ;;faust             ; dsp, but you get to keep your soul
-       ;;fortran           ; in FORTRAN, GOD is REAL (unless declared INTEGER)
-       ;;fsharp            ; ML stands for Microsoft's Language
-       ;;fstar             ; (dependent) types and (monadic) effects and Z3
-       ;;gdscript          ; the language you waited for
-       ;;(go +lsp)         ; the hipster dialect
-       ;;(graphql +lsp)    ; Give queries a REST
-       ;;(haskell +lsp)    ; a language that's lazier than I am
-       ;;hy                ; readability of scheme w/ speed of python
-       ;;idris             ; a language you can depend on
-       json              ; At least it ain't XML
-       ;;janet             ; Fun fact: Janet is me!
-       ;;(java +lsp)       ; the poster child for carpal tunnel syndrome
-       (javascript +lsp +tree-sitter)        ; all(hope(abandon(ye(who(enter(here))))))
-       ;;julia             ; a better, faster MATLAB
-       ;;kotlin            ; a better, slicker Java(Script)
-       ;;latex             ; writing papers in Emacs has never been so fun
-       ;;lean              ; for folks with too much to prove
-       ;;ledger            ; be audit you can be
-       (lua +lsp)               ; one-based indices? one-based indices
-       markdown          ; writing docs for people to ignore
-       ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
-       ;;ocaml             ; an objective camel
-       ;;odin              ; C, minus its footguns
-       (org +pretty)               ; organize your plain life in plain text
-       php               ; perl's insecure younger brother
-       ;;plantuml          ; diagrams for confusing people more
-       ;;graphviz          ; diagrams for confusing yourself even more
-       ;;purescript        ; javascript, but functional
-       python            ; beautiful is better than ugly
-       ;;qt                ; the 'cutest' gui framework ever
-       ;;racket            ; a DSL for DSLs
-       ;;raku              ; the artist formerly known as perl6
-       ;;rest              ; Emacs as a REST client
-       ;;rst               ; ReST in peace
-       ;;(ruby +rails)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
-       ;;(rust +lsp)       ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
-       ;;scala             ; java, but good
-       ;;(scheme +guile)   ; a fully conniving family of lisps
-       sh                ; she sells {ba,z,fi}sh shells on the C xor
+       ;;faust                         ; dsp, but you get to keep your soul
+       ;;fortran                       ; in FORTRAN, GOD is REAL (unless declared INTEGER)
+       ;;fsharp                        ; ML stands for Microsoft's Language
+       ;;fstar                         ; (dependent) types and (monadic) effects and Z3
+       ;;gdscript                      ; the language you waited for
+       ;;(go +lsp)                     ; the hipster dialect
+       ;;(graphql +lsp)                ; Give queries a REST
+       ;;(haskell +lsp)                ; a language that's lazier than I am
+       ;;hy                            ; readability of scheme w/ speed of python
+       ;;idris                         ; a language you can depend on
+       (json +tree-sitter)             ; At least it ain't XML
+       ;;janet                         ; Fun fact: Janet is me!
+       ;;(java +lsp)                   ; the poster child for carpal tunnel syndrome
+       (javascript +lsp +tree-sitter)  ; all(hope(abandon(ye(who(enter(here))))))
+       ;;julia                         ; a better, faster MATLAB
+       ;;kotlin                        ; a better, slicker Java(Script)
+       ;;latex                         ; writing papers in Emacs has never been so fun
+       ;;lean                          ; for folks with too much to prove
+       ;;ledger                        ; be audit you can be
+       (lua +lsp +tree-sitter)         ; one-based indices? one-based indices
+       (markdown +tree-sitter)         ; writing docs for people to ignore
+       ;;nim                           ; python + lisp at the speed of c
+       ;;nix                           ; I hereby declare "nix geht mehr!"
+       ;;ocaml                         ; an objective camel
+       ;;odin                          ; C, minus its footguns
+       (org +pretty)                   ; organize your plain life in plain text
+       (php +tree-sitter)              ; perl's insecure younger brother
+       ;;plantuml                      ; diagrams for confusing people more
+       ;;graphviz                      ; diagrams for confusing yourself even more
+       ;;purescript                    ; javascript, but functional
+       (python +tree-sitter)           ; beautiful is better than ugly
+       ;;qt                            ; the 'cutest' gui framework ever
+       ;;racket                        ; a DSL for DSLs
+       ;;raku                          ; the artist formerly known as perl6
+       ;;rest                          ; Emacs as a REST client
+       ;;rst                           ; ReST in peace
+       ;;(ruby +rails)                 ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       ;;(rust +lsp)                   ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
+       ;;scala                         ; java, but good
+       ;;(scheme +guile)               ; a fully conniving family of lisps
+       sh                              ; she sells {ba,z,fi}sh shells on the C xor
        ;;sml
-       ;;solidity          ; do you need a blockchain? No.
-       ;;swift             ; who asked for emoji variables?
-       ;;terra             ; Earth and Moon in alignment for performance.
-       (web +lsp)               ; the tubes
-       yaml              ; JSON, but readable
-       ;;zig               ; C, but simpler
+       ;;solidity                      ; do you need a blockchain? No.
+       ;;swift                         ; who asked for emoji variables?
+       ;;terra                         ; Earth and Moon in alignment for performance.
+       (web +lsp +tree-sitter)         ; the tubes
+       (yaml +tree-sitter)             ; JSON, but readable
+       ;;zig                           ; C, but simpler
 
        :email
        ;;(mu4e +org +gmail)
